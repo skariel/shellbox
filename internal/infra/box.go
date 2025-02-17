529e080ebe5cb674c44b72b12bd3da53a2432fcf
Your Name <you@example.com>
2025-02-17 10:34:15 +0000

    feat: Update box base script to automate Ubuntu VM setup with cloud-init and QEMU

--- internal/infra/box.go
+++ internal/infra/box.go
@@ -14,9 +14,58 @@ import (
 )
 
 const (
-	boxBaseScript = `#!/bin/bash
-sudo apt-get update -y
-sudo apt-get upgrade -y`
+    boxBaseScript = `#!/bin/bash
+
+echo "\$nrconf{restart} = 'a';" | sudo tee /etc/needrestart/conf.d/50-autorestart.conf
+sudo apt update
+sudo apt install qemu-kvm qemu-system libvirt-daemon-system libvirt-clients bridge-utils genisoimage whois libguestfs-tools -y
+
+sudo usermod -aG kvm,libvirt $USER
+sudo systemctl enable --now libvirtd
+
+mkdir -p ~/qemu-disks ~/qemu-memory
+
+wget https://cloud-images.ubuntu.com/releases/24.04/release/ubuntu-24.04-server-cloudimg-amd64.img
+cp ubuntu-24.04-server-cloudimg-amd64.img ~/qemu-disks/ubuntu-base.qcow2
+qemu-img resize ~/qemu-disks/ubuntu-base.qcow2 16G
+
+cat << 'EOF' > user-data 
+#cloud-config
+hostname: ubuntu
+users:
+  - name: ubuntu
+    passwd: '$6$rounds=4096$UFg6YrZy/onJUwol$cHMc9AgqYoyEZ3FnVGnnNk8C7mSitS43yOwLAshx6l9WR4FA5he6XUviVzR2D3YNaKCzSvFov8IQH6yHOVe7f.'
+    lock_passwd: false
+    shell: /bin/bash
+    sudo: ALL=(ALL) NOPASSWD:ALL
+package_update: true
+packages:
+  - openssh-server
+ssh_pwauth: true
+ssh:
+  install-server: yes
+  permit_root_login: false
+  password_authentication: true
+EOF
+
+cat << 'EOF' > meta-data
+instance-id: ubuntu-inst-1
+local-hostname: ubuntu
+EOF
+
+genisoimage -output ~/qemu-disks/cloud-init.iso -volid cidata -joliet -rock user-data meta-data
+
+sudo qemu-system-x86_64 \
+   -enable-kvm \
+   -m 4G \
+   -mem-prealloc \
+   -mem-path ~/qemu-memory/ubuntu-mem \
+   -smp 4 \
+   -cpu host \
+   -drive file=~/qemu-disks/ubuntu-base.qcow2,format=qcow2 \
+   -drive file=~/qemu-disks/cloud-init.iso,format=raw \
+   -nographic \
+   -nic user,model=virtio,hostfwd=tcp::2222-:22,dns=8.8.8.8`
 )
 
 func GenerateBoxInitScript() (string, error) {
