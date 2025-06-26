f7239a537d4355ac2cb56f1eca5f8280abf705ea
Your Name <you@example.com>
2025-06-26 20:39:48 +0000

    stuff

--- internal/infra/constants.go
+++ internal/infra/constants.go
@@ -141,8 +141,8 @@ const (
 
 // Persistent resource group for golden snapshots (shared across deployments)
 const (
-	GoldenSnapshotResourceGroup    = "shellbox-golden-images-21"
-	GlobalSharedStorageAccountName = "shellboxshared21"
+	GoldenSnapshotResourceGroup    = "shellbox-golden-images-22"
+	GlobalSharedStorageAccountName = "shellboxshared22"
 )
 
 // Timeout constants
